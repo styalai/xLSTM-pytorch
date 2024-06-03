34cb929be7c6ed9d72cb644be2d5ca3d472331b1
styalai <127979302+styalai@users.noreply.github.com>
2024-06-03 08:08:03 +0200

    Update xLSTM.py

--- xLSTM/xLSTM.py
+++ xLSTM/xLSTM.py
@@ -5,15 +5,15 @@ from xLSTM.mLSTMblock import mLSTMblock
 from xLSTM.sLSTMblock import sLSTMblock
 
 class xLSTM(nn.Module):
-    def __init__(self, layers, x_example, depth=4, factor=2):
+    def __init__(self, layers, x_example, depth=4, factor=2, conv=False):
         super(xLSTM, self).__init__()
 
         self.layers = nn.ModuleList()
         for layer_type in layers:
             if layer_type == 's':
-                layer = sLSTMblock(x_example, depth)
+                layer = sLSTMblock(x_example, depth, conv=conv)
             elif layer_type == 'm':
-                layer = mLSTMblock(x_example, factor, depth)
+                layer = mLSTMblock(x_example, factor, depth, conv=conv)
             else:
                 raise ValueError(f"Invalid layer type: {layer_type}. Choose 's' for sLSTM or 'm' for mLSTM.")
             self.layers.append(layer)
