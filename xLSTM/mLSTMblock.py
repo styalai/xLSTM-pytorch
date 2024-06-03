f679aeca2d131bced01bb9bac0db8540c3feaf15
styalai <127979302+styalai@users.noreply.github.com>
2024-06-03 11:25:05 +0200

    Update mLSTMblock.py

--- xLSTM/mLSTMblock.py
+++ xLSTM/mLSTMblock.py
@@ -14,7 +14,7 @@ class mLSTMblock(nn.Module):
         self.left = nn.Linear(self.input_size, self.hidden_size)
         self.right = nn.Linear(self.input_size, self.hidden_size)
         
-        self.conv = CausalConv1D(self.input_size, self.input_size, self.input_size) 
+        self.conv = CausalConv1D(self.hidden_size, self.hidden_size, self.input_size) 
         
         self.lskip = nn.Linear(self.hidden_size, self.hidden_size)
         
@@ -54,7 +54,7 @@ class mLSTMblock(nn.Module):
         right = F.silu(self.right(x)) # part right with just swish (silu) function
 
         left_left = left.transpose(1, 2)
-        print(self.input_size, left_left.shape)
+        print(self.input_size, left_left.shape, left.shape)
         left_left = F.silu(self.conv( left_left ).transpose(1, 2) )
         l_skip = self.lskip(left_left)
 
