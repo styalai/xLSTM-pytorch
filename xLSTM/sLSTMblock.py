7e1f8d521aeee4521e93fc23550568d042192543
styalai <127979302+styalai@users.noreply.github.com>
2024-06-04 18:25:13 +0200

    Update sLSTMblock.py

--- xLSTM/sLSTMblock.py
+++ xLSTM/sLSTMblock.py
@@ -23,6 +23,11 @@ class sLSTMblock(nn.Module):
         self.rf_gate = BlockDiagonal(self.input_size, self.input_size, depth, bias=False)
         self.ro_gate = BlockDiagonal(self.input_size, self.input_size, depth, bias=False)
         self.rz_gate = BlockDiagonal(self.input_size, self.input_size, depth, bias=False)
+
+        self.ln_i = nn.LayerNorm(self.input_size)
+        self.ln_f = nn.LayerNorm(self.input_size)
+        self.ln_o = nn.LayerNorm(self.input_size)
+        self.ln_z = nn.LayerNorm(self.input_size)
         
         self.GN = nn.LayerNorm(self.input_size)
         self.ln_c = nn.LayerNorm(self.input_size)
@@ -51,11 +56,11 @@ class sLSTMblock(nn.Module):
         # start sLSTM
         ht_1 = self.ht_1
         
-        i = torch.exp(self.i_gate(x_conv) + self.ri_gate(ht_1))
-        f = torch.exp(self.f_gate(x_conv) + self.rf_gate(ht_1))
+        i = torch.exp(self.ln_i( self.i_gate(x_conv) + self.ri_gate(ht_1) ) )
+        f = torch.exp( self.ln_f(self.f_gate(x_conv) + self.rf_gate(ht_1) ) )
         
-        o = torch.sigmoid(self.o_gate(x) + self.ro_gate(ht_1))
-        z = torch.tanh(self.z_gate(x) + self.rz_gate(ht_1))
+        o = torch.sigmoid( self.ln_o(self.o_gate(x) + self.ro_gate(ht_1) ) )
+        z = torch.tanh( self.ln_z(self.z_gate(x) + self.rz_gate(ht_1) ) )
         
         ct_1 = self.ct_1
         ct = f*ct_1 + i*z
